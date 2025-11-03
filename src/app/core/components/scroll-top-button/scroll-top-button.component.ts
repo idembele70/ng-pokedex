8399b26c65ac4943b3441011a4227b3d089ba0e9
Ibrahim Dembele <idembele70@gmail.com>
2025-11-03 19:30:41 +0100

    [#4] add search bar inputs, scroll-top btn & remove angular/material (#5)
    * feat: add search bar inputs & scroll-top btn; remove angular/material
    
    Refs: #4
    
    * feat: move to a signal based variable for isHidden & implement scroll to top event on click
    
    Refs: #4
    
    * style: add missing semicolon after closing brace
    
    ---------
    
    Co-authored-by: idembele70 <idembele70@gmail>

---
+++ src/app/core/components/scroll-top-button/scroll-top-button.component.ts
@@ -0,0 +1,111 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, NgZone, OnDestroy, OnInit, Renderer2, signal } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-scroll-top-button',
+  standalone: true,
+  imports: [
+    TranslatePipe,
+  ],
+  template: `
+  <button [title]="'scrollTopButton.title' | translate"
+  [attr.aria-hidden]="isHidden()"
+  [attr.focusable]="!isHidden()"
+  [hidden]="isHidden()"
+  (click)="scrollToTop($event)"
+  >
+    <svg color="white"
+      height="24"
+      width="24"
+      data-prefix="fas"
+      data-icon="chevron-up"
+      class="svg-inline--fa fa-chevron-up"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"><path fill="currentColor" d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"></path>
+    </svg>
+  </button>
+  `,
+  styles: `
+    button {
+      width: 48px;
+      height: 48px;
+      background-color: rgb(196, 196, 196);
+      border: 1px solid rgb(196, 196, 196);
+      opacity: 1;
+      border-radius: 50%;
+      position: fixed;
+      bottom: 30px;
+      left: 50%;
+      transform: translateX(-50%);
+      padding: 0px;
+      cursor: pointer;
+      transition: 1s;
+      visibility: visible;
+      z-index: 5;
+      outline: none;
+
+      &:hover {
+        background-color: rgb(196, 196, 196);
+      }
+      &:focus {
+        box-shadow: 0 0 0 0.25rem rgba(83, 118, 252, 0.5);
+      }
+    }
+  `,
+})
+export class ScrollTopButtonComponent implements OnInit, OnDestroy {
+  isHidden = signal<boolean>(true);
+  private unListenScroll?: () => void;
+  private scrollToTopTimeoutId?: ReturnType<typeof setTimeout>;
+  private readonly SCROLL_TO_TOP_DELAY = 500;
+
+  constructor(
+    private readonly renderer: Renderer2,
+    private readonly ngZone: NgZone,
+    @Inject(DOCUMENT) private readonly document: Document,
+  ) { }
+
+  scrollToTop(ev: Event) {
+    ev.preventDefault();
+    this.clearScrollToTopTimeout();
+    this.scrollToTopTimeoutId = setTimeout(() => {
+      this.document.scrollingElement?.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }, this.SCROLL_TO_TOP_DELAY);
+  }
+
+  ngOnInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      this.listenScroll();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unListenScroll?.();
+    this.clearScrollToTopTimeout();
+  }
+
+  private listenScroll() {
+    this.unListenScroll = this.renderer.listen(this.document, 'scroll', () => {
+      const scrollTop = this.document.documentElement.scrollTop;
+      if (scrollTop > 300 && this.isHidden()) {
+        this.ngZone.run(() => {
+          this.isHidden.set(false);
+        });
+      } else if (scrollTop < 300 && !this.isHidden()) {
+        this.ngZone.run(() => {
+          this.isHidden.set(true);
+        });
+      }
+    });
+  }
+
+  private clearScrollToTopTimeout() {
+    if (this.scrollToTopTimeoutId)
+      clearTimeout(this.scrollToTopTimeoutId);
+  }
+}
