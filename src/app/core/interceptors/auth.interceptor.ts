ba3cdfd6eb752fda14e8e861fbe8a45f85cb5c60
idembele70 <idembele70@gmail>
2025-11-24 20:23:25 +0100

    Enhance auth interceptor to handle refresh token errors and improve loader service state management

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -4,6 +4,8 @@ import { catchError, switchMap, throwError } from 'rxjs';
 import { JwtService } from '../services/jwt.service';
 import { RefreshTokenService } from '../services/refresh-token.service';
 import { HttpUtilities } from '../utilities/http.utilities';
+import { API_PATHS_TOKEN } from '../config/api-paths.config';
+import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   if (HttpUtilities.isExcluded(req.url)) {
@@ -12,19 +14,23 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const refreshTokenService = inject(RefreshTokenService);
   const jwtService = inject(JwtService);
   const tokenReq = addTokenToHeader(req, jwtService.getToken());
+  const apiPath = inject(API_PATHS_TOKEN);
+  const authService = inject(AuthService);
 
   return next(tokenReq).pipe(
     catchError((err: HttpErrorResponse) => {
-      if (err.status === 401) {
+      if (err.url?.endsWith(apiPath.AUTH.REFRESH_TOKEN)) {
+        authService.logout();
+        authService.setAuthVisibility(true);
+      } else if (err.status === 401) {
         return refreshTokenService.refreshToken().pipe(
           switchMap(({ accessToken }) => {
             const retryReq = addTokenToHeader(tokenReq, accessToken);
             return next(retryReq);
           })
         )
-      } else {
-        return throwError(() => err);
       }
+      return throwError(() => err);
     }),
   );
 };
